6e7055965b585a5677f07ffc30bbc0d3ebe81f46
ulivz <472590061@qq.com>
2018-03-25 17:09:02 +0800

    docs: add doc

--- README.md
+++ README.md
@@ -1,3 +1,37 @@
 # jquery-scrollable-overlay
 
-This library doesn't care how your overlay implements. It only does one thing. When your popup overlay can slide, it disables the background slide and is compatible with iOS and other mobile devices.
+This library doesn't care how you implement your overlay. It only does one thing. When your popup overlay can slide, it disables the background slide and is compatible with iOS and other mobile devices.
+
+
+# Usage
+
+This library is not published to NPM, you can use it directly:
+
+```html
+<script src="https://rawgit.com/ulivz/jquery-scrollable-overlay/master/index.js"></script>
+```
+
+```js
+const $overlay = $('/* You overlay selector */')
+$overlay.scrollableOverlay() // Use this plugin.
+
+$openIcon.on('click', () => $overlay.trigger('show')) // call when opening your overlay.
+$clostIcon.on('click', () => $overlay.trigger('close')) // call when closing your overlay.
+```
+
+
+## Contributing
+
+1. Fork it!
+2. Create your feature branch: `git checkout -b my-new-feature`
+3. Commit your changes: `git commit -am 'Add some feature'`
+4. Push to the branch: `git push origin my-new-feature`
+5. Submit a pull request :D
+
+
+## Author
+
+**jquery-scrollable-overlay** © [ulivz](https://github.com/ULIVZ), Released under the [MIT](./LICENSE) License.<br>
+Authored and maintained by ulivz with help from contributors ([list](https://github.com/ULIVZ/jquery-scrollable-overlay/contributors)).
+
+> [github.com/ulivz](https://github.com/ulivz) · GitHub [@ulivz](https://github.com/ULIVZ)
--- index.js
+++ index.js
@@ -1,43 +1,44 @@
+'use strict';
+
 (function ($) {
+  var lastWindowScrollTop = 0;
+  var originalOverlayPadding = 0;
 
-	var lastWindowScrollTop = 0
-	var originalOverlayPadding = 0
+  function isIPhone() {
+    return /iPhone/.test(navigator.userAgent);
+  }
 
-	function isIPhone() {
-		return /iPhone/.test(navigator.userAgent)
-	}
+  function isPad() {
+    return /iPad/.test(navigator.userAgent);
+  }
 
-	function isPad() {
-		return /iPad/.test(navigator.userAgent)
-	}
+  function fixedBody() {
+    lastWindowScrollTop = $(window).scrollTop();
+    $('body').addClass('noscroll');
+    $('body').css('top', '-' + lastWindowScrollTop + 'px');
+  }
 
-	function fixedBody() {
-		lastWindowScrollTop = $(window).scrollTop()
-		$('body').addClass('noscroll')
-		$('body').css('top', '-' + lastWindowScrollTop + 'px')
-	}
+  function looseBody() {
+    $('body').removeClass('noscroll');
+    $('body').css('top', '');
+    window.scrollTo(0, lastWindowScrollTop);
+  }
 
-	function looseBody() {
-		$('body').removeClass('noscroll')
-		$('body').css('top', '')
-		window.scrollTo(0, lastWindowScrollTop)
-	}
-
-	$.fn.scrollableOverlay = function () {
-		var $this = $(this)
-		this.on('show', function () {
-			$this.removeClass('hidden')
-			originalOverlayPadding = parseInt($this.css('padding-bottom'))
-			// Fix iPad would show navbar(44px) when fixed
-			// Fix iPhone would show navbar(44px) and toolbar(44px) when fixed
-			var tradeOff = isIPhone() ? 88 : isPad() ? 44 : 0
-			$this.css('padding-bottom', (originalOverlayPadding + tradeOff) + 'px')
-			fixedBody()
-		})
-		this.on('hide', function () {
-			$this.addClass('hidden')
-			$this.css('padding-bottom', originalOverlayPadding + 'px')
-			looseBody()
-		})
-	};
+  $.fn.scrollableOverlay = function () {
+    var $this = $(this);
+    this.on('show', function () {
+      $this.removeClass('hidden');
+      originalOverlayPadding = parseInt($this.css('padding-bottom'));
+      // Fix iPad would show navbar(44px) when fixed
+      // Fix iPhone would show navbar(44px) and toolbar(44px) when fixed
+      var tradeOff = isIPhone() ? 88 : isPad() ? 44 : 0;
+      $this.css('padding-bottom', (originalOverlayPadding + tradeOff) + 'px');
+      fixedBody();
+    })
+    this.on('hide', function () {
+      $this.addClass('hidden');
+      $this.css('padding-bottom', originalOverlayPadding + 'px');
+      looseBody();
+    })
+  };
 })(jQuery);
