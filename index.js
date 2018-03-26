1414c87bcde41cf431bd1ffd57f830d8c9c1a0e3
ulivz <472590061@qq.com>
2018-03-26 19:30:55 +0800

    refactor: code

--- index.js
+++ index.js
@@ -2,19 +2,10 @@
 
 (function ($) {
 
-  function isIPhone() {
-    return /iPhone/.test(userAgent);
-  }
-
-  function isPad() {
-    return /iPad/.test(userAgent);
-  }
-
   var lastWindowScrollTop = 0,
-      originalOverlayPaddingBottom = 0,
       userAgent = navigator.userAgent,
       $body = $('body'),
-      isIOS = isIPhone() || isPad(),
+      isIOS = /iPhone|iPad/.test(userAgent),
       NO_SCROLL_CLASS = isIOS
           ? 'ios-noscroll'
           : 'non-ios-noscroll';
@@ -38,22 +29,8 @@
   }
 
   $.fn.scrollableOverlay = function () {
-    var $this = $(this);
-    this.on('show', function () {
-      $this.removeClass('hidden');
-      if (isIOS) {
-        originalOverlayPaddingBottom = parseInt($this.css('padding-bottom'));
-        var tradeOff = isIPhone() ? 88 : isPad() ? 44 : 0;
-        $this.css('padding-bottom', (originalOverlayPaddingBottom + tradeOff) + 'px');
-      }
-      fixedBody();
-    })
-    this.on('hide', function () {
-      $this.addClass('hidden');
-      if (isIOS) {
-        $this.css('padding-bottom', originalOverlayPaddingBottom + 'px');
-      }
-      looseBody();
-    })
+    this.on('show', fixedBody)
+    this.on('hide', looseBody)
   };
+  
 })(jQuery);
